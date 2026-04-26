449e19a055a8eda3c504122e8a2eb98cbd1d7a58
YOURKEYMASTER <quiroykr@gmail.com>
2026-04-26 08:42:27 +0800

    Update and rename data to config.py

---
+++ config.py
@@ -0,0 +1,27 @@
+# config.py - YKA SOVEREIGN KNOWLEDGE CORE
+MANIFEST_DATA = {
+    "PIONEER_ZERO": {
+        "Caudal_Spread": 180.3,
+        "Spine_Angle": 0.0,
+        "Ray_Count": 11,
+        "Anal_Ratio": "1.0:1.5"
+    },
+    "LEX_AGENT": {
+        "Name": "LEX",
+        "Designation": "Linguist & Visual Auditor",
+        "Mandates": ["Synthetic Filter", "Lexicon Mapping", "Visual Refraction"]
+    }
+}
+
+TAXONOMY_MAP = {
+    "Solid Red": "全红 (Quán Hóng)",
+    "Solid Black": "全黑 (Quán Hēi)",
+    "Metallic/Dragon": "铠甲龙 (Kǎijiǎ Lóng)",
+    "Black Samurai": "黑武士 (Hēi Wǔshì)"
+}
+
+PHENOTYPES = {
+    "HMPK": "180.3° Spread; 11 Primary Rays",
+    "HM": "Overlapping fins; 1:1.5 Trapezoid ratio",
+    "CT": "50% Webbing reduction"
+}
